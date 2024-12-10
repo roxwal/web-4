615cc88c1205af523e44f008318603d5623b58ab
roxwal <163902062+roxwal@users.noreply.github.com>
2024-12-10 23:07:25 +0800

    Add files via upload

---
+++ pemograman wab sudah berubah/materi css/css.js
@@ -0,0 +1,20 @@
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+function toggleSidebar() {
+    sidebar.classList.toggle('close'); // Menambahkan atau menghapus kelas "close"
+    toggleButton.classList.toggle('rotate'); // Menambahkan atau menghapus rotasi pada tombol (opsional)
+}
+
+function toggleSubMenu(button) {
+    button.nextElementSibling.classList.toggle('show'); // Toggle submenu
+    // Jika sidebar tertutup dan sub-menu diklik, buka sidebar
+    if (sidebar.classList.contains('close')) {
+        sidebar.classList.remove('close');
+        toggleButton.classList.remove('rotate');
+    }
+}
+
+function goBack() {
+    window.location.replace('/projek uts/projek uts/index.html');
+  }
